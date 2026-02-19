74fe49b665c6216a2761daa29f853e2d8fc15c68
Nahorny.Code <engs-gabrielnahorny@camporeal.edu.br>
2026-02-19 00:13:58 -0300

    atividade 1 aula 2

--- aula 2/Atividade1.java
+++ aula 2/Atividade1.java
@@ -3,9 +3,11 @@ public class Atividade1 {
         double peso = 82;
         double altura = 1.64;
         double imc = 0;
+        
         System.out.println("Calculando IMC.....");
         imc = (altura * altura);
         imc = peso / imc;
         System.out.println(imc);
+    
     }
 }
