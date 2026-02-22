6e65a9854712bca6485fa670e770ce1dcaa42237
Nahorny.Code <engs-gabrielnahorny@camporeal.edu.br>
2026-02-21 23:57:52 -0300

    atividade aula 3 POO

--- aula3/Atv5.java
+++ aula3/Atv5.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
 public class Atv5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        int j = 0;
+        int qnt = 0;
+        int ast = 0;
+        System.out.println("Digite a quantidade de linhas da escada");
+        qnt = sc.nextInt();
+
+        for (i = 0; i < qnt; i++){
+            ast++;
+                for (j = 0; j < ast; j++){
+                System.out.print("*");
+                }
+            System.out.println();
+        }
+        
+    }
+    
     
 }
---
+++ aula3/aula.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class aula {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+        int contador = 0;
+
+        while (contador < 5) {
+            System.out.println("Contador" + contador);
+            contador++;
+        }
+        do {
+            System.out.println("1.jogar");
+            System.out.println("2.sair");
+            opcao = sc.nextInt();
+
+        } while (opcao != 2);
+
+        for (int a = 0; a < 5; a++) {
+            System.out.println("valor de i:" + a);
+        }
+
+        int[] numeros = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 25;
+
+        String[] frutas = { "maça", "banana" };
+        System.out.println(frutas[1]);
+
+        int[][] matriz = new int[3][3];
+        matriz[0][0] = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(matriz[3][3] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
